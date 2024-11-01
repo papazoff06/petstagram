151732f89075ee37a94a4722c9905ce72820974a
papazoff06 <134053962+papazoff06@users.noreply.github.com>
2024-11-01 18:59:18 +0200

    Created Comment and Like model

--- petstagram/common/admin.py
+++ petstagram/common/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from petstagram.common.models import Like, Comment
+from petstagram.pets.models import Pet
+
+
 # Register your models here.
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
